18e28b96c5999bfbff563f00c3ceb06921af2b6c
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-03-22 06:58:47 +0900

    ✨ feat: 문자열 반복 API 구현

---
+++ blog/src/main/java/com/example/blog/controller/StringController.java
@@ -0,0 +1,21 @@
+package com.example.blog.controller;
+
+import com.example.blog.dto.StringRequest;
+import com.example.blog.dto.StringResponse;
+import com.example.blog.service.StringService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StringController {
+
+  private final StringService stringService;
+
+  @PostMapping("/string/repeat")
+  public StringResponse repeat(@RequestBody StringRequest request) {
+    return stringService.repeat(request.getValue());
+  }
+}
