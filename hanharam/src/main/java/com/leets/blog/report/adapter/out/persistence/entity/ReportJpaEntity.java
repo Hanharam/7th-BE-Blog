9334c44e74de7570616a21459b9553a705ec5b59
Hanharam <hanharamm@naver.com>
2026-04-28 23:32:05 +0900

    feat: report 엔티티 생성

---
+++ hanharam/src/main/java/com/leets/blog/report/adapter/out/persistence/entity/ReportJpaEntity.java
@@ -0,0 +1,70 @@
+package com.leets.blog.report.adapter.out.persistence.entity;
+
+import com.leets.blog.common.BaseEntity;
+import com.leets.blog.report.domain.Report;
+import com.leets.blog.report.domain.enums.ReportStatus;
+import com.leets.blog.report.domain.enums.ReportTargetType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "report")
+public class ReportJpaEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "reporter_id", nullable = false)
+    private Long reporterId;
+
+    @Column(name = "target_type", nullable = false)
+    private ReportTargetType targetType;
+
+    @Column(name = "target_id", nullable = false)
+    private Long targetId;
+
+    @Column(name = "report_status", nullable = false)
+    private ReportStatus reportStatus;
+
+    @Column(name = "reason", nullable = false, length = 250)
+    private String reason;
+
+    @Builder
+    private ReportJpaEntity(Long reporterId, ReportTargetType targetType, Long targetId, ReportStatus reportStatus, String reason) {
+        this.reporterId = reporterId;
+        this.targetType = targetType;
+        this.targetId = targetId;
+        this.reportStatus = reportStatus;
+        this.reason = reason;
+    }
+
+    // Domain -> JPA Entity
+    public static ReportJpaEntity from(Report report) {
+        return ReportJpaEntity.builder()
+                .reporterId(report.getReporterId())
+                .targetType(report.getTargetType())
+                .targetId(report.getTargetId())
+                .reportStatus(report.getReportStatus())
+                .reason(report.getReason())
+                .build();
+    }
+
+    // JPA Entity -> Domain
+    public Report toDomain() {
+        return Report.reconstruct(
+                new Report.ReportId(this.id),
+                this.reporterId,
+                this.targetType,
+                this.targetId,
+                this.reportStatus,
+                this.reason
+        );
+    }
+
+}
