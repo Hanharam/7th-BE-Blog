fa3fdacf3a48bbb15b59a600f83f3df58e42dd03
Hanharam <hanharamm@naver.com>
2026-05-05 18:17:46 +0900

    feat: swagger 연동 및 버전 업데이트

--- hanharam/build.gradle.kts
+++ hanharam/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // Swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.17")
 }
 
 tasks.withType<Test> {
